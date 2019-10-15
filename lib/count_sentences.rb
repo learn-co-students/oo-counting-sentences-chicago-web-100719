5872114d2ff183d501d4fa888b9f59472d441d37
Nathan Alex <nate745@gmail.com>
2019-10-15 11:53:09 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include? "."
   end
 
   def question?
-
+    self.include? "?"
   end
 
   def exclamation?
-
+    self.include? "!"
   end
 
   def count_sentences
-
+    arr = self.split(/[.?!]/)
+    arr.reject!{
+      |string| string == ""
+    }
+    p arr.length
   end
 end
